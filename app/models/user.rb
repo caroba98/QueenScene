37ccab01547aeeaa5631a413fa64f34a98380dfe
Caroline Baumeister <caroline@baumeister-dv.de>
2023-11-20 16:53:16 +0100

    Setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :queens
-  validates :name, presence: true
-  validates :name, uniqueness: true
-  validates :address, presence: true
-  validates :phone, presence: true
-  validates :email, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :bookings
+         has_many :queens
+         validates :name, presence: true
+         validates :name, uniqueness: true
+         validates :address, presence: true
+         validates :phone, presence: true
+         validates :email, presence: true
 end
