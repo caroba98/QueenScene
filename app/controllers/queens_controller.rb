24bb7fc203b5b2d82a9401260bf7fcd0f67a3ec6
Bence Kiss <bencekissde@gmail.com>
2023-11-21 14:18:42 +0100

    Added new page for queens, updated routes and made some patches

--- app/controllers/queens_controller.rb
+++ app/controllers/queens_controller.rb
@@ -6,4 +6,29 @@ class QueensController < ApplicationController
   def show
     @queen = Queen.find(params[:id])
   end
+
+
+
+  def new
+    @queen = Queen.new
+  end
+
+  def create
+    @queen = Queen.new(queen_params)
+    if @queen.save
+      redirect_to queen_path(@queen), notice: 'Drag Queen profile created successfully. Work Bitch! '
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def queen_params
+    params.require(:queen).permit(:name, :location, :price, :skills)
+  end
+
+
+
+  
 end
